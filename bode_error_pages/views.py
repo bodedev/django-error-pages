53abd373613f9f89d279f980b8eaaba47fc28cda
Cícero Raupp Rolim <ironworld@gmail.com>
2017-09-28 16:37:40 -0300

    Fixing a problem with template path on view.

--- bode_error_pages/views.py
+++ bode_error_pages/views.py
@@ -6,14 +6,14 @@ from django.views.generic.base import TemplateView
 
 class Error403View(TemplateView):
 
-    template_name = 'website/errors/403.html'
+    template_name = 'errors/403.html'
 
 
 class Error404View(TemplateView):
 
-    template_name = 'website/errors/404.html'
+    template_name = 'errors/404.html'
 
 
 class Error500View(TemplateView):
 
-    template_name = 'website/errors/500.html'
+    template_name = 'errors/500.html'

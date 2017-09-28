7b711d1d05dd2254fe669c80b7dad7075a4b4b9e
Cícero Raupp Rolim <ironworld@gmail.com>
2017-09-28 16:37:14 -0300

    Removing unused file.

--- bode_error_pages/urls.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-from website.error_views import Error404View, Error403View, Error500View
-
-handler403 = Error403View.as_view()
-handler404 = Error404View.as_view()
-handler500 = Error500View.as_view()

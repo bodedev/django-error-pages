086d4c3128d34b35295d1a7a2d6eac6a5670197e
Cícero Raupp Rolim <ironworld@gmail.com>
2017-10-13 11:01:57 -0300

    Lets go to production

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open(('README.rst'), encoding='utf-8') as f:
 
 setup(
     name='bode-django-error-pages',
-    version='0.0.1',
+    version='0.0.2',
     description='This is a simple project to handle http errors responses to Bode Django Projects',
     long_description=long_description,
 
